41e225403d608b331125973739768475b279d23f
Vladislav <radostmoyaaa@mail.ru>
2021-03-25 11:51:44 +0300

    Created locators

---
+++ homework-1/locators.py
@@ -0,0 +1,17 @@
+from selenium.webdriver.common.by import By
+
+# Локаторы для SignIn
+# Кнопка "Войти" в header-е
+SIGN1_BTN_LOCATOR = (By.XPATH, './/div[contains(text(), "Войти") and contains(@class, "responseHead-module-button")]')
+
+# Кнопка "Войти" в auth форме
+SIGN2_BTN_LOCATOR = (By.XPATH, './/div[contains(text(), "Войти") and contains(@class,"authForm-module-button")] ')
+
+# Поле email в auth форме
+LOG_LOCATOR = (By.NAME, 'email')
+
+# Поле password в auth форме
+PASS_LOCATOR = (By.NAME, 'password')
+
+# UserName
+USERNAME_LOCATOR = (By.XPATH, './/div[contains(@class, "right-module-userNameWrap")]')
